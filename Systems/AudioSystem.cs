da047059e38689b774dbda9aedff935cfd677fba
themaaaadcow <calebrmiller04@gmail.com>
2026-05-08 03:54:24 -0400

    Added walking audio as required by issue #7, and oh my god it is glorious. Also added an audio framework so I can play other sounds later :)

---
+++ Systems/AudioSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Media;
+
+namespace app.Systems
+{
+    public class AudioSystem
+    {
+        private SoundPlayer _player;
+
+        public void InitializeSound(string audio)
+        {
+            _player = new SoundPlayer(audio);
+            _player.Load();
+        }
+
+        public void Play()
+        {
+            _player?.Play();
+        }
+        public void Stop()
+        {
+            _player.Stop();
+        }
+    }
+}
